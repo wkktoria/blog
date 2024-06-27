afbd02cbc6818dfe1cdbcb261cd1dd703334a2db
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-27 21:51:19 +0200

    Make adding new posts possible

--- src/main/java/io/github/wkktoria/blog/controllers/PostController.java
+++ src/main/java/io/github/wkktoria/blog/controllers/PostController.java
@@ -1,20 +1,26 @@
 package io.github.wkktoria.blog.controllers;
 
+import io.github.wkktoria.blog.models.Account;
 import io.github.wkktoria.blog.models.Post;
+import io.github.wkktoria.blog.services.AccountService;
 import io.github.wkktoria.blog.services.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
 @Controller
 public class PostController {
     private final PostService postService;
+    private final AccountService accountService;
 
-    public PostController(final PostService postService) {
+    public PostController(final PostService postService, final AccountService accountService) {
         this.postService = postService;
+        this.accountService = accountService;
     }
 
     @GetMapping("/posts/{id}")
@@ -28,5 +34,25 @@ public class PostController {
         } else {
             return "404";
         }
+    }
+
+    @GetMapping("/posts/new")
+    public String createNewPost(Model model) {
+        Optional<Account> optionalAccount = accountService.findByEmail("user@domain.com");
+
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post", post);
+            return "post_new";
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/new")
+    public String saveNewPost(@ModelAttribute Post post) {
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
     }
 }
--- src/main/java/io/github/wkktoria/blog/services/AccountService.java
+++ src/main/java/io/github/wkktoria/blog/services/AccountService.java
@@ -4,6 +4,8 @@ import io.github.wkktoria.blog.models.Account;
 import io.github.wkktoria.blog.repositories.AccountRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AccountService {
     private final AccountRepository accountRepository;
@@ -14,5 +16,9 @@ public class AccountService {
 
     public Account save(final Account account) {
         return accountRepository.save(account);
+    }
+
+    public Optional<Account> findByEmail(final String email) {
+        return accountRepository.findOneByEmail(email);
     }
 }
