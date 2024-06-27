30ed697d58edfb12392145662e32e365188fc66d
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-27 16:38:04 +0200

    Create post service

---
+++ src/main/java/io/github/wkktoria/blog/services/PostService.java
@@ -0,0 +1,34 @@
+package io.github.wkktoria.blog.services;
+
+import io.github.wkktoria.blog.models.Post;
+import io.github.wkktoria.blog.repositories.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(final PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Optional<Post> getById(final Long id) {
+        return postRepository.findById(id);
+    }
+
+    public List<Post> getAll() {
+        return postRepository.findAll();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == null) {
+            post.setCreatedAt(LocalDateTime.now());
+        }
+
+        return postRepository.save(post);
+    }
+}
